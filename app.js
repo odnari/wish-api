3aa6773c9501af38c95b2ec19cb9b001f0211ec4
Nick <rezoh.c@gmail.com>
2018-07-24 22:49:23 +0300

    fix: envs

--- app.js
+++ app.js
@@ -21,20 +21,31 @@ app.get('/', (req, res) => res.send(`${process.env.APP_NAME} API`))
 app.use('/api/users', userRouter)
 app.use('/api/wishes', wishRouter)
 
-const options = {
-  cert: fs.readFileSync('./cert.pem'),
-  key: fs.readFileSync('./key.pem'),
-  passphrase: '12qwaszx'
-}
-
-app.listen(process.env.PORT + 1, (err) => {
-  if (err) {
-    console.error('[express] ' + err.message)
-  } else {
-    console.info('[express] running at ' + process.env.PORT)
+if (process.env.ENV === 'development') {
+  const options = {
+    cert: fs.readFileSync('./cert.pem'),
+    key: fs.readFileSync('./key.pem'),
+    passphrase: '12qwaszx'
   }
-})
-https.createServer(options, app).listen(process.env.PORT)
+
+  app.listen(process.env.PORT + 1, (err) => {
+    if (err) {
+      console.error('[express] ' + err.message)
+    } else {
+      console.info('[express] running at ' + process.env.PORT)
+    }
+  })
+
+  https.createServer(options, app).listen(process.env.PORT)
+} else {
+  app.listen(process.env.PORT, (err) => {
+    if (err) {
+      console.error('[express] ' + err.message)
+    } else {
+      console.info('[express] running at ' + process.env.PORT)
+    }
+  })
+}
 
 mailer.verify((err) => {
   if (err) {
