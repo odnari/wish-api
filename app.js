9fe1d315c9e8417ef4dd556f1ed29fdd1d2812f2
Nick <rezoh.c@gmail.com>
2018-07-26 18:51:32 +0300

    chore: allow cors

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ const {router: wishRouter} = require('./wishes/routes')
 const app = express()
 
 app.use(bodyParser.json())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.send(`${process.env.APP_NAME} API`))
