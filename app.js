112f6c5da3f3b0454f0f2705b2731a996f2f50bd
Nick <rezoh.c@gmail.com>
2018-07-26 20:53:16 +0300

    fix: cors for x-auth header

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authorization')
   res.header('Access-Control-Expose-Headers', 'X-Authorization')
   next()
 })
