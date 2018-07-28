08ee640c2c7e90dc3333386b095c908b32210d6e
Nick <rezoh.c@gmail.com>
2018-07-28 22:25:24 +0300

    fix: disable cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authorization')
   res.header('Access-Control-Expose-Headers', 'X-Authorization')
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
