1774f8128190f26d027cb8c51ecdf56c4a58b444
Nick <rezoh.c@gmail.com>
2018-09-09 20:53:07 +0300

    fix: cache time

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,6 @@
-const { mongoose } = require('./../db/mongoose')
+const { mongoose, cachegoose } = require('./../db/mongoose')
 
 afterAll(() => {
   mongoose.connection.close()
+  cachegoose.clearCache()
 })
