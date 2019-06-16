289ab13f82f4245826fe6304a1e3d7d5f08b9727
Nick <rezoh.c@gmail.com>
2019-06-16 18:22:25 +0300

    fix: bring back verification

--- mail/helper.js
+++ mail/helper.js
@@ -1,4 +1,4 @@
-const {mailer} = require('./mailer')
+const { mailer } = require('./mailer')
 
 const baseMessage = {
   from: process.env.EMAIL_FROM,
@@ -6,18 +6,18 @@ const baseMessage = {
 }
 
 const sendEmailVerificationMail = (to, { token }) => {
-  // const message = Object.assign({}, baseMessage, {
-  //   to,
-  //   subject: `${process.env.APP_NAME}: E-mail verification`,
-  //   template: 'email_verification',
-  //   context: {
-  //     appName: process.env.APP_NAME,
-  //     homepage_url: `${process.env.BASE_URL}:${process.env.PORT}/`,
-  //     verify_token_url: `${process.env.BASE_URL}:${process.env.PORT}/api/users/verify/${token}`
-  //   }
-  // })
-  //
-  // return mailer.sendMail(message)
+  const message = Object.assign({}, baseMessage, {
+    to,
+    subject: `${process.env.APP_NAME}: E-mail verification`,
+    template: 'email_verification',
+    context: {
+      appName: process.env.APP_NAME,
+      homepage_url: `${process.env.BASE_URL}:${process.env.PORT}/`,
+      verify_token_url: `${process.env.BASE_URL}:${process.env.PORT}/api/users/verify/${token}`
+    }
+  })
+
+  return mailer.sendMail(message)
 }
 
 module.exports = {
